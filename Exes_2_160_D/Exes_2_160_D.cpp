9f2bbd27f6ca90269327b0db78a72b65a29edbfb
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-12 10:32:02 +0700

    menambah sortting

--- Exes_2_160_D/Exes_2_160_D.cpp
+++ Exes_2_160_D/Exes_2_160_D.cpp
@@ -1,9 +1,8 @@
-
-#include <iostream>
 #include <iostream>
 using namespace std;
 
-int sa[29]; //jumlah array maksimum 
+
+int sa[5]; //jumlah array maksimum 
 int n; // number of element in the array
 int i; // index of array element
 
@@ -11,7 +10,7 @@ void input() {
     while (true) {
         cout << "Enter the numner of element in the array = ";
         cin >> n;
-        if ((n > 0) && (n <= 92))
+        if ((n > 0) && (n <= 29))
             break;
         else
             cout << "\nArray minimum adalah 1 dan maksimum adalah 29\n";
@@ -28,28 +27,52 @@ void input() {
     }
 }
 
+void SelectionSort() {
+    for (int ER = 0; ER < n - 1; ER++)
+    {
+        int min_index = ER;
+        for (int i = ER + 1; i <= n - 1; i++)
+        {
+            
+            if (sa[i] < sa[min_index]) {
+                min_index = i;
+                int temp;
+                temp = sa[ER];
+                sa[ER] = sa[min_index];
+                sa[min_index] = temp;
+                
+            }
+        }
+    }
+}
+
 void search() {
     int ersa = 0;
     int rohman = n - 1;
     int element;
 
-    cout << "/nMasukkan Angka yang ingin dicari = " << endl;
+    cout << "\nMasukkan Angka yang ingin dicari = ";
     cin >> element;
 
-    while (rohman <= ersa) {
+    while (ersa <= rohman) {
         int mid = (ersa + rohman) / 2;
-        if (sa[mid]
+        if (sa[mid] == element) {
+            cout << "\nFound" << endl;
+            return;
+        }
+        else if (element < sa[mid]) 
+            rohman = mid - 1;
+        
+        else 
+            ersa = mid + 1;
     }
-   
-
- 
-
-
-    
+    cout << "\nNot Found" << endl;;
 }
 
 int main()
 {
-    void input();
-    void search();
+    input();
+    SelectionSort();
+    search();
+    
 }
