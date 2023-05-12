7cc08f03a81b244eceb2645a10e2557c4ef652cd
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-12 09:38:24 +0700

    Membuat void search

--- Exes_2_160_D/Exes_2_160_D.cpp
+++ Exes_2_160_D/Exes_2_160_D.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-int ersa[92]; //jumlah array maksimum 
+int sa[29]; //jumlah array maksimum 
 int n; // number of element in the array
 int i; // index of array element
 
@@ -14,7 +14,7 @@ void input() {
         if ((n > 0) && (n <= 92))
             break;
         else
-            cout << "\nArray minimum adalah 1 dan maksimum adalah 92\n";
+            cout << "\nArray minimum adalah 1 dan maksimum adalah 29\n";
     }
 
     //Accept array elements
@@ -24,11 +24,32 @@ void input() {
     for (int i = 0; i < n; i++)
     {
         cout << "<" << (i + 1) << ">";
-        cin >> arr[i];
+        cin >> sa[i];
     }
+}
+
+void search() {
+    int ersa = 0;
+    int rohman = n - 1;
+    int element;
+
+    cout << "/nMasukkan Angka yang ingin dicari = " << endl;
+    cin >> element;
+
+    while (rohman <= ersa) {
+        int mid = (ersa + rohman) / 2;
+        if (sa[mid]
+    }
+   
+
+ 
+
+
+    
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    void input();
+    void search();
 }
